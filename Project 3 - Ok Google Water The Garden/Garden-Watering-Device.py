5d7e376d1f3b016da5097e5ec90619274ff0e77b
Les Pounder <lespounder@gmail.com>
2017-10-07 16:42:51 +0100

    Changed pin to GPIO27 as 17 was glitchy

--- Project 3 - Ok Google Water The Garden/Garden-Watering-Device.py
+++ Project 3 - Ok Google Water The Garden/Garden-Watering-Device.py
@@ -4,7 +4,7 @@ import time
 import paho.mqtt.client as mqtt
 
 
-relay = DigitalOutputDevice(17)
+relay = DigitalOutputDevice(27)
 
 def on_connect(client, userdata, flags, rc):  
         print("Connected with result code "+str(rc))
@@ -12,7 +12,7 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):  
         message = str(msg.payload)
-        message = message[2:(len(message))]
+        message = message[2:(len(message)-1)]
         print(message)
         if(message=="water garden"):
                 print("Watering Garden")
@@ -24,5 +24,5 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()  
 client.on_connect = on_connect  
 client.on_message = on_message  
-client.connect("192.168.0.7", 1883, 60)
+client.connect("BROKER IP ADDRESS", 1883, 60)
 client.loop_forever() 
